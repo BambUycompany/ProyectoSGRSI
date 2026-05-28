b0c2b5d236ce1eae568911c0882f96aed26f6111
ezequiel rodriguez <ezequielobedrodriguez@gmail.com>
2026-05-27 23:01:26 -0300

    v.1 creacion de aparatdo Registro Sala resolves #3

---
+++ js/creacionTicket.js
@@ -0,0 +1,7 @@
+document.getElementById("btnCrearTicket").onclick = () => {
+    document.getElementById("creacionTicket").style.display = "block";
+};
+
+function cerrarModal() {
+    document.getElementById("creacionTicket").style.display = "none";
+}
